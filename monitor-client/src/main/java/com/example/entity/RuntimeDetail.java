0f20d533ca6e20dc55b1ed8013130f14197b2fc5
yanqi <3224848830@qq.com>
2024-05-24 23:43:40 +0800

    feat: get runtime data

---
+++ monitor-client/src/main/java/com/example/entity/RuntimeDetail.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class RuntimeDetail {
+    long timestamp;
+    double cpuUsage;
+    double memoryUsage;
+    double diskUsage;
+    double networkUpload;
+    double networkDownload;
+    double diskRead;
+    double diskWrite;
+}
--- monitor-client/src/main/java/com/example/utils/MonitorUtils.java
+++ monitor-client/src/main/java/com/example/utils/MonitorUtils.java
@@ -1,9 +1,12 @@
 package com.example.utils;
 
 import com.example.entity.BaseDetail;
+import com.example.entity.RuntimeDetail;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.HWDiskStore;
 import oshi.hardware.HardwareAbstractionLayer;
 import oshi.hardware.NetworkIF;
 import oshi.software.os.OperatingSystem;
@@ -12,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.NetworkInterface;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Objects;
 import java.util.Properties;
 
@@ -38,6 +42,63 @@ public class MonitorUtils {
                 .setMemory(memory)
                 .setDisk(diskSize)
                 .setIp(ip);
+    }
+
+    public RuntimeDetail monitorRuntimeDetail() {
+        double statisticTime = 0.5;
+        try {
+            HardwareAbstractionLayer hardware = info.getHardware();
+            NetworkIF networkInterface = Objects.requireNonNull(this.findNetworkInterface(hardware));
+            CentralProcessor processor = hardware.getProcessor();
+            double upload = networkInterface.getBytesSent();
+            double download = networkInterface.getBytesRecv();
+            double read = hardware.getDiskStores().stream().mapToLong(HWDiskStore::getReadBytes).sum();
+            double write = hardware.getDiskStores().stream().mapToLong(HWDiskStore::getWriteBytes).sum();
+            long[] ticks = processor.getSystemCpuLoadTicks();
+            Thread.sleep((long) (statisticTime * 1000));
+            networkInterface = Objects.requireNonNull(this.findNetworkInterface(hardware));
+            upload = (networkInterface.getBytesSent() - upload) / statisticTime;
+            download = (networkInterface.getBytesRecv() - download) / statisticTime;
+            read = (hardware.getDiskStores().stream().mapToLong(HWDiskStore::getReadBytes).sum() - read) / statisticTime;
+            write = (hardware.getDiskStores().stream().mapToLong(HWDiskStore::getWriteBytes).sum() - write) / statisticTime;
+            double memory = (hardware.getMemory().getTotal() - hardware.getMemory().getAvailable()) / 1024.0 / 1024 / 1024;
+            double disk = Arrays.stream(File.listRoots())
+                    .mapToLong(file -> file.getTotalSpace() - file.getFreeSpace()).sum() / 1024.0 / 1024 / 1024;
+            return new RuntimeDetail()
+                    .setCpuUsage(this.calculateCpuUsage(processor, ticks))
+                    .setMemoryUsage(memory)
+                    .setDiskUsage(disk)
+                    .setNetworkUpload(upload / 1024)
+                    .setNetworkDownload(download / 1024)
+                    .setDiskRead(read / 1024 / 1024)
+                    .setDiskWrite(write / 1024 / 1024)
+                    .setTimestamp(new Date().getTime());
+        } catch (Exception e) {
+            log.error("读取网络接口信息时出错", e);
+        }
+        return null;
+    }
+
+    private double calculateCpuUsage(CentralProcessor processor, long[] prevTicks) {
+        long[] ticks = processor.getSystemCpuLoadTicks();
+        long nice = ticks[CentralProcessor.TickType.NICE.getIndex()]
+                - prevTicks[CentralProcessor.TickType.NICE.getIndex()];
+        long irq = ticks[CentralProcessor.TickType.IRQ.getIndex()]
+                - prevTicks[CentralProcessor.TickType.IRQ.getIndex()];
+        long softIrq = ticks[CentralProcessor.TickType.SOFTIRQ.getIndex()]
+                - prevTicks[CentralProcessor.TickType.SOFTIRQ.getIndex()];
+        long steal = ticks[CentralProcessor.TickType.STEAL.getIndex()]
+                - prevTicks[CentralProcessor.TickType.STEAL.getIndex()];
+        long cSys = ticks[CentralProcessor.TickType.SYSTEM.getIndex()]
+                - prevTicks[CentralProcessor.TickType.SYSTEM.getIndex()];
+        long cUser = ticks[CentralProcessor.TickType.USER.getIndex()]
+                - prevTicks[CentralProcessor.TickType.USER.getIndex()];
+        long ioWait = ticks[CentralProcessor.TickType.IOWAIT.getIndex()]
+                - prevTicks[CentralProcessor.TickType.IOWAIT.getIndex()];
+        long idle = ticks[CentralProcessor.TickType.IDLE.getIndex()]
+                - prevTicks[CentralProcessor.TickType.IDLE.getIndex()];
+        long totalCpu = cUser + nice + cSys + idle + ioWait + irq + softIrq + steal;
+        return (cSys + cUser) * 1.0 / totalCpu;
     }
 
     private NetworkIF findNetworkInterface(HardwareAbstractionLayer hardware) {
