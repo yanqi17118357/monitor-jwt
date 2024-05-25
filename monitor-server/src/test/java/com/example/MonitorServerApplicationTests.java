0589c433acd0b80a025483df5d55aecbba61de17
yanqi <3224848830@qq.com>
2024-05-25 22:31:59 +0800

    chore: add passwordEncoder test

--- monitor-server/src/test/java/com/example/MonitorServerApplicationTests.java
+++ monitor-server/src/test/java/com/example/MonitorServerApplicationTests.java
@@ -2,12 +2,17 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class MonitorServerApplicationTests {
 
+    PasswordEncoder encoder = new BCryptPasswordEncoder();
+
     @Test
     void contextLoads() {
-
+        String rawPassword = "00000000";
+        System.out.println(encoder.encode(rawPassword));
     }
 }
